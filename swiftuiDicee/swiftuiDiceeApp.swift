213916700a987e6ac64cfa24b780524c9237f4c0
tramalho <thiagofacul@gmail.com>
2021-10-09 23:25:45 -0300

    init

---
+++ swiftuiDicee/swiftuiDiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiDiceeApp.swift
+//  swiftuiDicee
+//
+//  Created by Thiago Antonio Ramalho on 09/10/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiDiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
